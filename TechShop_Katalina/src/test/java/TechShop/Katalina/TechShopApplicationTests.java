08d9a546f63b0cec3069f63a55d146c4a540a4ef
Katalina Zúñiga <katazuniga27@gmail.com>
2026-05-20 20:09:50 -0600

    Commit 1
    Creación del proyecto semana 1 y 2

---
+++ TechShop_Katalina/src/test/java/TechShop/Katalina/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package TechShop.Katalina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
