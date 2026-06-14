e74226a2488b02dfb55d999cbeb9a0bebbefe8c4
Katalina Zúñiga <katazuniga27@gmail.com>
2026-06-14 15:13:52 -0600

    Commit 8
    Semana 5

---
+++ TechShop_Katalina/src/main/java/TechShop/Katalina/repository/ProductoRepository.java
@@ -0,0 +1,11 @@
+package TechShop.Katalina.repository;
+
+import TechShop.Katalina.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    public List<Producto> findByActivoTrue();
+}
