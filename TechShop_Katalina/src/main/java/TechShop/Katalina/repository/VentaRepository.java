ba47245c866f2f5c02ad083a0c4bcfedec5483a4
Katalina Zúñiga <katazuniga27@gmail.com>
2026-08-05 01:46:54 -0600

    commit 13
    semana 12

---
+++ TechShop_Katalina/src/main/java/TechShop/Katalina/repository/VentaRepository.java
@@ -0,0 +1,8 @@
+package TechShop.Katalina.repository;
+
+import TechShop.Katalina.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer>{
+    
+}
